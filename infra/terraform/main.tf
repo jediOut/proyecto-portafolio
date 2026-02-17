019854366d5ee54a6f51fd682fea99deb27d59b8
Jedidiah Viera <jedi.viera@gmail.com>
2026-02-16 20:12:47 -0600

    fix role reference

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -16,9 +16,8 @@
 # }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
+ role = "cloud_pos_lambda_role"
+ }
 
 resource "aws_lambda_function" "api" {
   function_name = "cloud-pos-api"
