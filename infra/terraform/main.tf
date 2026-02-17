5e3825c36723870a791e726be1120b18b347c463
Jedidiah Viera <jedi.viera@gmail.com>
2026-02-16 21:16:55 -0600

    restore lambda role managed by terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,22 +6,22 @@ terraform {
   }
 }
 
-# resource "aws_iam_role" "lambda_role" {
-#   name = "cloud_pos_lambda_role"
-#
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "lambda.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+ resource "aws_iam_role" "lambda_role" {
+   name = "cloud_pos_lambda_role"
+
+   assume_role_policy = jsonencode({
+     Version = "2012-10-17"
+     Statement = [
+       {
+         Action = "sts:AssumeRole"
+         Effect = "Allow"
+         Principal = {
+           Service = "lambda.amazonaws.com"
+         }
+       }
+     ]
+   })
+ }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
  role = "cloud_pos_lambda_role"
@@ -37,7 +37,7 @@ resource "aws_lambda_function" "api" {
   handler       = "lambda.handler"
   runtime       = "nodejs18.x"
 
-  role =  "arn:aws:iam::687337999212:role/cloud_pos_lambda_role"
+  role =  "aws_iam_role.lambda_role.arn"
 }
 
 resource "aws_apigatewayv2_api" "http_api" {
