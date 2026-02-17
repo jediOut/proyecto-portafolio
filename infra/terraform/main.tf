b1df9b3d4c1bd3358a1a5366f8e8fc11863aa5fd
Jedidiah Viera <jedi.viera@gmail.com>
2026-02-16 20:01:10 -0600

    use exixting lambda role

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,19 +1,19 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "cloud_pos_lambda_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+# resource "aws_iam_role" "lambda_role" {
+#   name = "cloud_pos_lambda_role"
+#
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "lambda.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_role.name
@@ -29,7 +29,7 @@ resource "aws_lambda_function" "api" {
   handler       = "lambda.handler"
   runtime       = "nodejs18.x"
 
-  role = aws_iam_role.lambda_role.arn
+  role = role = "arn:aws:iam::687337999212:role/cloud_pos_lambda_role"
 }
 
 resource "aws_apigatewayv2_api" "http_api" {
